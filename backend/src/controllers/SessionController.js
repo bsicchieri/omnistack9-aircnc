c5b4e92014a651b0ad2a10324deb2b041c549d40
bsicchieri <brunosicchieri@hotmail.com>
2019-10-02 18:58:34 -0300

    backend

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,12 @@
+//index (retornar uma listagem de sessões)
+//show (listar uma única sessão)
+//store (criar uma sessão)
+//update (alterar uma sessão)
+//destroy (remover uma sessão)
+
+module.exports = {
+    store(req, res) {
+        //return res.json({ message: "Hello World" })
+        const { email } = req.body; //const email = req.body.email; 
+    }
+};
