c47d418e2327ff263e22e6af951d246d322531ea
bsicchieri <brunosicchieri@hotmail.com>
2019-10-03 16:51:11 -0300

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
 //importando o react (criando um estado)
-import React, { useState } from 'react';
-
-//importando a api
-import api from './services/api';
+import React from 'react';
 
 //importando arquivo css
 import './App.css';
@@ -10,39 +7,16 @@ import './App.css';
 //importando uma imagem
 import logo from './assets/logo.svg';
 
-//retornando um conteúdo html
+//importando as rotas
+import Routes from './routes';
+
 function App() {
-  const [email, setEmail] = useState('');
-
-  async function handleSubmit(event) {
-    event.preventDefault()
-
-    const response = await api.post('/sessions', { email });
-
-    console.log(response);
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
 
     <div className="content">
-      <p>
-        Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-      </p>
-
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">E-MAIL *</label>
-      <input 
-        id="email"
-        type="email"
-        placeholder="Seu melhor e-mail"
-        value={email}
-        onChange={event => setEmail(event.target.value)}
-      />
-
-      <button className="btn" type="submit">Entrar</button>
-    </form>
+      <Routes />
     </div>
     </div>
   );
