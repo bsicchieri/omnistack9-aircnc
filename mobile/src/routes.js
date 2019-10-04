69a0ee8038407659229c46768f38cb3d540a30cc
bsicchieri <brunosicchieri@hotmail.com>
2019-10-04 20:37:35 -0300

    mobile

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,3 +1,15 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
-const Routes = createAppContainer()
+import Login from './pages/Login';
+import List from './pages/List';
+import Book from './pages/Book';
+
+const Routes = createAppContainer(
+    createSwitchNavigator({
+        Login,
+        List,
+        Book
+    })
+);
+
+export default Routes;
